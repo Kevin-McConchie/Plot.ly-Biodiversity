6e1af41165f278390eab702e60125062bd561b21
Kevin McConchie <Kevin.McConchie@outlook.com>
2022-08-20 08:31:05 +1000

    Update app.js

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,54 +1,50 @@
 
- // Use D3 to select the dropdown menu for when a selection is made
-var selection = d3.select("#selDataset");
-
-// Use D3 to create an event handler
-d3.selectAll("body").on("change", init);
-
 // dropdown select function
-function init() {
+function dropMenu() {
 
-// Fetch the JSON data and console log it
-d3.json("samples.json").then(function(data) {
+// create id dropdown list
+  var selection = d3.select("#selDataset");
 
-// create Test subject dropdown list
-  var sampleNames = data.names; 
-    sampleNames.forEach((sample)=> {
+  d3.json("samples.json").then((data) =>{
+    var sampleNames = data.names; 
+    sampleNames.forEach((name)=> {
       selection
       .append("option")
-      .text(sample)
-      .property("value",sample);
-    })
- 
-
-// Populate Demographic info panel
-
-// var dropdownMenu = d3.select("#selDataset");
-// var dataset = dropdownMenu.property("value");
-// var info=[]
-// console.log(info)
-
-
-
-// var id = d3.select("#onchange");
-///////////////////////////////
-var id = 940
-var metadata= data.metadata;
-var info =metadata.filter(sampleId=>sampleId.id == id)
-console.log(info)
-console.log(metadata);
-
-var infoPanel = d3.select('#sample-metadata').html("")
-
-for (const [key,value] of Object.entries(info)){
-  infoPanel.append("i").text(`${key}`,`${value}`)
-  console.log(`${key},${value}`);
-}
-
+      .text(name)
+      .property("value",name);
   });
-
+});
 }
-init();
+
+dropMenu()
+
+
+
+
+///////////////////////////////
+// var id = 940
+// var metadata= data.metadata;
+// var info =metadata.filter(sampleId=>sampleId.id === id)
+// console.log(info)
+// console.log(metadata);
+
+// var infoPanel = d3.select('#sample-metadata').html("")
+
+// // Object.entries(sample).forEach(([key,value])=>{
+// //   row = sampleMetadata.append("h6").text(`${key}:${value}`);
+// // console.log(`key:${key} and value:${value}`)
+
+// for (const [key,value] of Object.entries(info)){
+//   infoPanel.append("sample-metadata").text(`${key}`,`${value}`)
+//   console.log(`${key},${value}`);
+// }
+
+
+
+//   });
+
+
+
 
 
 // BAR PLOT Slice the first 10 objects for plotting from 2-06
